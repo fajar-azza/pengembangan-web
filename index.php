5e493cea837bbdfd866cc041bdd2e99798b1b028
fajar-azza <fajarazzanajwa@gmail.com>
2024-11-04 14:52:57 +0700

    penambahan halaman index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Fajar</title>
+</head>
+<body>
+    
+    <div style="text-align:center">
+    <h1>
+        ini adalah header
+    </h1>
+        
+    </div>
+    <div>
+        <p>
+            rencananya ini adalah halaman yang berbeda
+        </p>
+    </div>
+</body>
+</html>
